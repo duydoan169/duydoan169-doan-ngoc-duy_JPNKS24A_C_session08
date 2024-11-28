736b633c894291a03740bc77edf59a908004bc8b
duydoan169 <hanh250470@gmail.com>
2024-11-29 01:35:41 +0700

    s8 bài 1

---
+++ s8b1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int array[2][3]={
+                    {1,2,3},
+                    {4,5,6}
+                    };
+    for(int i=1; i>=0; i--){
+        for(int j=2; j>=0; j--){
+            printf("%d\n", array[i][j]);
+        }
+    }
+
+    return 0;
+}
